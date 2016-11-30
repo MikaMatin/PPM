1b9db7116d6b707f243aafedfe83d3a9cafe0975
Mahyar Matin Azad <mrmatinazad@mail.lipscomb.edu>
2016-11-30 17:00:06 -0600

    Image attributes

---
+++ Image.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MikaMatin on 11/17/16.
+ */
+public abstract class Image {
+    protected String magic;
+    protected int width;
+    protected int height;
+    protected int depth;
+}
